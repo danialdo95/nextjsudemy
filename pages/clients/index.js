dcea95aa62f913a9c202bb7469b17fb073156415
Muhammad Danial <danialdev95@gmail.com>
2023-10-25 10:09:37 +0800

    Add navigation to to dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,26 @@
+import Link from "next/link";
+
 function ClientsPage() {
-    return (
-        <div>
-            <h1>The Clients Page</h1>
-        </div>
-    );
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "danny", name: "Danial" },
+  ];
+
+  return (
+    <div>
+      <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={{
+                pathname: '/clients/[id]'
+            }}>{client.name}</Link>
+            {/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ClientsPage;
