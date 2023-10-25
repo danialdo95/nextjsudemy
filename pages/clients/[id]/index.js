6eac4b7c51b9baef757270ebb432cfb474edb4ce
Muhammad Danial <danialdev95@gmail.com>
2023-10-25 18:05:57 +0800

    Add imperative dynamic navigation and custom 404 page

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -7,8 +7,17 @@ function ClientProjectPage() {
 
     console.log(router.query);
 
+    function loadProjectHandler() {
+        // load data..
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: {id: 'max', clientprojectid: 'projecta'}
+        });
+    }
+
     return <div>
         <h1>The Projects of a Given Client</h1>
+        <button onClick={loadProjectHandler}>Load project A</button>
     </div>
 }
 
